7cfd2364a59129c6875131f18120e8c7b6f54076
user <user@email.com>
2021-08-05 13:57:13 +0300

    Autocommit 2021-08-05 13:57:13

--- new_file.R
+++ new_file.R
@@ -1,12 +1,11 @@
-### ЭТО ДОЛЖНА БЫТЬ САМАЯ ПОСЛЕДНЯЯ ЧАСТЬ В ОТЧЕТЕ 
-# здесь мы автоматически записываем изменения в git
+# здесь я просто тестировал, что новые файлы будут добавляться в историю
 # таким способом можно будет отслеживать все модификации расчетов
 if(!git2r::in_repository()){
     # если мы не в репозитории, то создаем его
     git2r::init()
 }
 # задаем пользователя, от которого будем коммитить
-git2r::config(user.name = "datamary", user.email = "datamary@admitad.com")
+git2r::config(user.name = "user", user.email = "user@email.com")
 if(!file.exists(".gitignore")){
     # если не существует файла .gitignore, то создаем его
     writeLines(c(".Rproj.user", ".Rhistory", ".RData", ".Ruserdata", "data/", "secret"),
@@ -18,10 +17,10 @@ git_status = git2r::status()
 # проверяем, есть ли что коммитить
 if(length(git_status)>0){
     #  если есть что коммитить, то коммитим
-    version = git2r::commit(message = paste("Autocommit", Sys.time()), all = TRUE)
+    commit_info = git2r::commit(message = paste("Autocommit", Sys.time()), all = TRUE)
 } else {
     # если все по-старому, то в качестве версии берем последний коммит
-    version = git2r::last_commit()
+    commit_info = git2r::last_commit()
 }
-version = format(version)
-cat("Версия", version, "\n")
+commit_info = format(commit_info)
+cat("Версия", commit_info, "\n")
